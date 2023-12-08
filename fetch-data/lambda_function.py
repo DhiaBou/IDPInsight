3b1fce2e46bb4c6a82d0b7c1feea219620f21ddd
Dhia Bouassida <dhia.bouassida@tngtech.com>
2023-12-08 17:41:37 +0100

    merge branches

--- fetch-data/lambda_function.py
+++ fetch-data/lambda_function.py
@@ -1,4 +1,3 @@
-
 import boto3
 import csv
 from hdx.utilities.easy_logging import setup_logging
@@ -7,17 +6,14 @@ from hdx.data.dataset import Dataset
 
 
 def lambda_handler(event, context):
-    s3 = boto3.client('s3')
-    bucket_name = 'devgurus-raw-data'
-    file_name = 'gothdx.csv'
-    csv_content = [['4']]
+    s3 = boto3.client("s3")
+    bucket_name = "devgurus-raw-data"
+    file_name = "gothdx.csv"
+    csv_content = [["4"]]
 
-    with open('/tmp/' + file_name, 'w', newline='') as file:
+    with open("/tmp/" + file_name, "w", newline="") as file:
         writer = csv.writer(file)
         writer.writerows(csv_content)
 
-    s3.upload_file('/tmp/' + file_name, bucket_name, file_name)
-    return {
-        'statusCode': 200,
-        'body': 'File created and uploaded successfully.'
-    }
+    s3.upload_file("/tmp/" + file_name, bucket_name, file_name)
+    return {"statusCode": 200, "body": "File created and uploaded successfully."}
--- process-data/lambda_function.py
+++ process-data/lambda_function.py
@@ -2,18 +2,16 @@ import csv
 import pandas
 import boto3
 
-def lambda_handler(event, context):
-    s3 = boto3.client('s3')
-    bucket_name = 'devgurus-processed-data'
-    file_name = 'gothdx.csv'
-    csv_content = [['4']]
 
-    with open('/tmp/' + file_name, 'w', newline='') as file:
+def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+    bucket_name = "devgurus-processed-data"
+    file_name = "gothdx.csv"
+    csv_content = [["4"]]
+
+    with open("/tmp/" + file_name, "w", newline="") as file:
         writer = csv.writer(file)
         writer.writerows(csv_content)
 
-    s3.upload_file('/tmp/' + file_name, bucket_name, file_name)
-    return {
-        'statusCode': 200,
-        'body': 'File created and uploaded successfully.'
-    }
+    s3.upload_file("/tmp/" + file_name, bucket_name, file_name)
+    return {"statusCode": 200, "body": "File created and uploaded successfully."}
