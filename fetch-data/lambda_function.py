dad4c100922c7eba70b20cf7f5e7157edb26227b
ge64qij <boris.trifonov@tum.de>
2023-12-11 23:06:55 +0100

    New_branch to current stage

--- fetch-data/lambda_function.py
+++ fetch-data/lambda_function.py
@@ -6,21 +6,12 @@ from hdx.data.dataset import Dataset
 from hdx.utilities.downloader import Download
 from hdx.utilities.easy_logging import setup_logging
 
+import requests
+import os
+
 s3_bucket = "devgurus-raw-data"
 s3 = boto3.client("s3")
-
-
-# def stream_path(self, path: str, errormsg: str):
-#    try:
-#        for chunk in self.response.iter_content(chunk_size=10240):
-#            if chunk:
-#                s3.upload_fileobj(io.BytesIO(chunk), s3_bucket, path)
-#        return path
-#    except Exception:
-#        pass
-
-
-# hdx.utilities.downloader.Download.stream_path = stream_path
+s3_resource = boto3.resource("s3")
 
 
 def lambda_handler(event, context):
@@ -47,9 +38,21 @@ def download_all_resources_for_dataset(dataset_id, country_name):
     resources = Dataset.get_all_resources([dataset])
     path = "/tmp/" + country_name + "/"
     for resource in resources:
-        url, path = resource.download(path)
-        url_components = path.split("/")
-        file_name = url_components[len(url_components) - 1]
-        s3.Object(s3_bucket, file_name).upload_file(path)
+        download_url = resource.data.get("url", None)
+        file_name = resource.data.get("name", None)
+        file_type = resource.get_file_type()
+        file_extension = "." + file_type
 
-        print("Resource URL %s downloaded to %s\n" % (url, path))
+        # Check if file type is contained in the name
+        # If not the case, then add it
+        if not file_extension in file_name:
+            file_name = file_name + file_extension
+
+        file_path = os.path.join(path, file_name)
+
+        response = requests.get(download_url)
+        if response.status_code == 200:
+            s3_resource.Object(s3_bucket, file_path).put(Body=response.content)
+
+        # url, path = resource.download(path)
+        # print("Resource URL %s downloaded to %s\n" % (download_url, path))
