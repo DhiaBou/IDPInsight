7dd2787f56d16dca5c36ff06a0669bd1016ae58c
Lukas Neef <lukas.neef@tum.de>
2023-12-20 21:28:40 +0100

    moved hxl-parser into process data

--- process-data/hxl-parser.py
+++ process-data/hxl-parser.py
@@ -5,13 +5,10 @@ from hdx.data.organization import Organization
 from hdx.api.locations import Locations
 import pandas as pd
 import regex as re
-import boto3
+import tempfile as tf
 import io
 import os
 
-
-s3_o = "devgurus-clean-data"
-s3_cl = boto3.client("s3")
 
 #pattern for HXL
 pattern = r"#.*\+"
@@ -24,59 +21,58 @@ def search_rows(row):
 
     filtered = list(filter(lambda x : reg.match(x[1]), to_filter))
     return filtered
-
-
-
     
 def match_hxl(file_path):
         
     # get sheet names
     xlsx_file = pd.ExcelFile(file_path)
     names = xlsx_file.sheet_names
+    print(file_path)
         
     # iterate over sheets
     for name in names:
         file = pd.read_excel(file_path, sheet_name = name, header = None)
-            
+        
+        print(name)
         # check if HXL appears 
         for i,r in enumerate(file.values.astype(str), start=0):
             header_cols = search_rows(list(r))
+            print(header_cols)
                 
-            if header_cols != []:
-                indices = [ii for ii,_ in header_cols]
-                index_header = pd.Index(indices)
+            if header_cols:
+                indices = {ii:h for ii,h in header_cols}
+                index_header = pd.Index(indices.keys())
                     
                 #drop first <i> rows and returns columns with HXL tag
-                clean = file[index_header].drop(index = [ii for ii in range(i)])
-                return clean
-                
-                # continue with next sheet after 5 rows
-            if i == 4:
+                clean = file[index_header].drop(index = [ii for ii in range(i + 1)])
+                clean.rename(columns = indices, inplace = True)
+                yield clean
                 break
-    return None
-    
+                # continue with next sheet after 10 rows
+            if i == 9:
+                break    
 
-def lambda_handler(event, context):
-    bucket_name = event['Records'][0]['s3']['bucket']['name']
-    file_key = event['Records'][0]['s3']['object']['key']
-    
-    # not an .xlsx file
-    if not file_key.endswith('.xlsx'):
-        return
-    
-    file = s3.get_object(Bucket=bucket_name, Key=file_key)
-        
-    clean_data = match_hxl(io.BytesIO(obj['Body'].read()))
-    
-    # No columns found
-    if clean_data is None:
-        return
-    
-    output = io.StringIO()
-    name = file_key.replace('.xlsx', '') + ".csv"
-    clean_data.to_csv(output, index = None, header = False)
-    s3_cl.put_object(Bucket=s3_o, Key=name, Body=output.getvalue())
-    
+def match_hxl_list(file_path):
+    return list(match_hxl(file_path))
+
+
+# transform HXL to csv
+def to_file(file_path: str, dir: str):
+
+    for clean in match_hxl(file_path):
+
+        outname = file_path.split('/')[-1].replace('.', '-')
+
+        _,path = tf.mkstemp(suffix = '.csv', prefix=outname, dir = dir)
+
+        clean.to_csv(path, index = None)
+
+# clean all files in directory
+def parse_all(dirname, outdir):
+    for _, _, files in os.walk(dirname):
+        for f in files:
+            fullpath = dirname + '/' + f
+            to_file(fullpath, outdir)
     
     
     
