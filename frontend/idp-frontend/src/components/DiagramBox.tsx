64bde542c13e722790fc2785a5928cbf73e574f9
ge64qij <boris.trifonov@tum.de>
2024-01-04 13:29:52 +0100

    changes in frontend

--- frontend/idp-frontend/src/components/DiagramBox.tsx
+++ frontend/idp-frontend/src/components/DiagramBox.tsx
@@ -4,14 +4,13 @@ import { CountryButtonProps } from "../utils/types";
 function DiagramBox(props: CountryButtonProps) {
     if(props.countryName === 'sudan') {
         return (
-            <div className="diagram-container">
-                <div className="image-container">
-                    <img className="image" src="/sudan_1.png" alt={props.countryName} />
-                </div>
-                <div className="image-container">
-                    <img className="image" src="/sudan_2.png" alt={props.countryName} />
-                </div>
-            </div>
+        <div className="diagram-container">
+            <iframe
+                width="960"
+                height="720"
+                src="https://eu-west-1.quicksight.aws.amazon.com/sn/embed/share/accounts/122662047093/dashboards/c192f767-b772-4fe3-a63a-2f418e85c780?directory_alias=nl-wfp">
+            </iframe>
+        </div>
         );
     } else if(props.countryName === 'colombia') {
         return (
