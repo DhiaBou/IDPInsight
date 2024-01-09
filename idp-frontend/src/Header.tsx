609abaad657ebacc61cca002145de03e4f5a4695
DhiaBou <dhiabouassida@gmail.com>
2024-01-09 17:44:20 +0100

    include the visualization

--- idp-frontend/src/Header.tsx
+++ idp-frontend/src/Header.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import { Container, Navbar, ListGroup } from 'react-bootstrap';
-
-
+import React from 'react'
+import { Container, Navbar, ListGroup } from 'react-bootstrap'
 
 const Header: React.FC = () => {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="/">My Logo</Navbar.Brand>
-      </Container>
-    </Navbar>
-  );
-};
+	return (
+		<Navbar bg='light' expand='lg'>
+			<Container>
+				<Navbar.Brand href='/'>My Logo</Navbar.Brand>
+			</Container>
+		</Navbar>
+	)
+}
 
- export default Header;
+export default Header
