fcf2e5a4eaae11ab9914f744b7c8ce9193efdc5c
karim <karim.triki1999@gmail.com>
2024-01-11 01:18:19 +0100

    Added the option for pie charts and stacked bar charts

--- idp-frontend/src/fileVisualizer/ChartsComponent.tsx
+++ idp-frontend/src/fileVisualizer/ChartsComponent.tsx
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from 'react'
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, PieChart, Pie, Cell } from 'recharts'
 
 interface MyGroupedBarChartProps {
    data: { [key: string]: any }[]
 }
 
+const COLORS = [
+  '#0088FE', // Blue
+  '#FFBB28', // Yellow
+  '#00C49F', // Aqua Green
+  '#C71585', // MediumVioletRed
+  '#FF8042', // Orange
+  '#6A5ACD', // SlateBlue
+  '#D2691E', // Chocolate
+  '#20B2AA', // LightSeaGreen
+  '#FF6347', // Tomato
+  '#B8860B', // DarkGoldenRod
+  '#FF1493', // DeepPink
+  '#2E8B57', // SeaGreen
+  '#FF4500', // OrangeRed
+  '#778899', // LightSlateGray
+  '#A52A2A', // Brown
+];
 const aggregateData = (data: { [key: string]: any }[], groupByKey: string, valueKey: string) => {
    const result: { [key: string]: number } = {}
 
@@ -21,6 +38,43 @@ const aggregateData = (data: { [key: string]: any }[], groupByKey: string, value
 
    return Object.entries(result).map(([name, value]) => ({ name, value }))
 }
+
+interface BarData {
+    name: string;
+    [key: string]: string | number; // Allows the 'name' key to be string, all others must be number
+}
+
+const aggregateDataForStackedBarChart = (
+    data: any[],
+    groupByKey: string,
+    valueKey: string,
+    stackByKey: string
+): BarData[] => {
+    const result: { [group: string]: { [stack: string]: number } } = {};
+
+    data.forEach((item) => {
+        const groupValue = item[groupByKey];
+        const stackValue = item[stackByKey];
+        const value = parseInt(item[valueKey], 10) || 0;
+
+        if (!result[groupValue]) {
+            result[groupValue] = {};
+        }
+
+        if (!result[groupValue][stackValue]) {
+            result[groupValue][stackValue] = 0;
+        }
+        result[groupValue][stackValue] += value;
+    });
+    return Object.entries(result).map(([name, stacks]): BarData => {
+        const barData: BarData = { name };
+        for (const stack in stacks) {
+            barData[stack] = stacks[stack];
+        }
+        return barData;
+    });
+};
+
 
 interface SelectorProps {
    id: string
@@ -46,11 +100,11 @@ const Selector: React.FC<SelectorProps> = ({ id, label, value, options, onChange
    </div>
 )
 interface BarChartProps {
-   data: any[]
+    data: any[]
 }
 
 const GroupedBarChart: React.FC<BarChartProps> = ({ data }) => (
-   <BarChart width={500} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+   <BarChart width={800} height={600} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
       <CartesianGrid strokeDasharray='3 3' />
       <XAxis dataKey='name' />
       <YAxis />
@@ -59,7 +113,64 @@ const GroupedBarChart: React.FC<BarChartProps> = ({ data }) => (
       <Bar dataKey='value' fill='#8884d8' />
    </BarChart>
 )
+
+const StackedBarChart: React.FC<BarChartProps> = ({ data }) => {
+    const stackKeySet = new Set<string>();
+    data.forEach(item => {
+        Object.keys(item).forEach(key => {
+            if (key !== 'name') {
+                stackKeySet.add(key);
+            }
+        });
+    });
+    const stackKeys = Array.from(stackKeySet);
+
+    return (
+        <BarChart width={800} height={600} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            {stackKeys.map((key, index) => (
+                <Bar key={key} dataKey={key} stackId="a" fill={COLORS[index % COLORS.length]} />
+            ))}
+        </BarChart>
+    );
+};
+
+interface PieChartProps {
+    data: any[];
+}
+
+const GroupedPieChart: React.FC<PieChartProps> = ({ data }) => {
+    const LABEL_THRESHOLD = 0.02;
+    return (
+        <PieChart width={1200} height={800}>
+            <Pie
+                data={data}
+                cx={350}
+                cy={300}
+                labelLine={false}
+                label={({ name, percent }) => percent > LABEL_THRESHOLD ? `${name} ${(percent * 100).toFixed(0)}%` : ''}
+                outerRadius={200}
+                fill="#8884d8"
+                dataKey="value"
+            >
+                {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+            </Pie>
+            <Tooltip />
+        </PieChart>
+   );
+};
+
+
 const MyGroupedBarChart: React.FC<MyGroupedBarChartProps> = ({ data }) => {
+    const chartTypes = ['GroupedBarChart', 'PieChart', 'StackedBarChart'];
+    const [chartType, setChartType] = useState<string>(chartTypes[0]);
+    const [stackByKey, setStackByKey] = useState<string>('');
    const [groupByKey, setGroupByKey] = useState<string>('')
    const [valueKey, setValueKey] = useState<string>('')
    const [processedData, setProcessedData] = useState<any[]>([])
@@ -67,11 +178,34 @@ const MyGroupedBarChart: React.FC<MyGroupedBarChartProps> = ({ data }) => {
    const keys = data.length > 0 ? Object.keys(data[0]) : []
 
    const handleGenerateChart = () => {
-      if (groupByKey && valueKey) {
-         const newData = aggregateData(data, groupByKey, valueKey)
-         setProcessedData(newData)
-      }
-   }
+       if (groupByKey && valueKey) {
+           let newData;
+           if (chartType === 'StackedBarChart' && stackByKey) {
+               newData = aggregateDataForStackedBarChart(data, groupByKey, valueKey, stackByKey);
+           } else {
+               newData = aggregateData(data, groupByKey, valueKey);
+           }
+           setProcessedData(newData);
+       }
+   };
+
+
+   const renderChart = () => {
+       let chartData = [];
+
+       if (chartType === 'GroupedBarChart') {
+           chartData = aggregateData(data, groupByKey, valueKey);
+           return <GroupedBarChart data={chartData} />;
+       } else if (chartType === 'PieChart') {
+           chartData = aggregateData(data, groupByKey, valueKey);
+           return <GroupedPieChart data={chartData} />;
+       } else if (chartType === 'StackedBarChart' && stackByKey) {
+           chartData = aggregateDataForStackedBarChart(data, groupByKey, valueKey, stackByKey);
+           return <StackedBarChart data={chartData} />;
+       }
+
+       return null;
+   };
 
    return (
       <div>
@@ -94,13 +228,33 @@ const MyGroupedBarChart: React.FC<MyGroupedBarChartProps> = ({ data }) => {
                   onChange={setValueKey}
                />
             </div>
+             {chartType === 'StackedBarChart' && (
+            <div className='col'>
+                <Selector
+                    id='stackByKeySelect'
+                    label='Stack by:'
+                    value={stackByKey}
+                    options={keys.filter(key => key !== groupByKey && key !== valueKey)}
+                    onChange={setStackByKey}
+                />
+            </div>
+            )}
+             <div className='col'>
+                <Selector
+                    id='chartTypeSelect'
+                    label='Chart type:'
+                    value={chartType}
+                    options={chartTypes}
+                    onChange={setChartType}
+                />
+             </div>
             <div className='col'>
                <button className='btn btn-primary btn-sm my-4' onClick={handleGenerateChart}>
                   Generate Chart
                </button>
             </div>
          </div>
-         {processedData.length > 0 && <GroupedBarChart data={processedData} />}
+         {processedData.length > 0 && renderChart()}
       </div>
    )
 }
