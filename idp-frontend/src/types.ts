c5e2632aa7eee0f9ed07c5e6635cf3f5adc0851c
Miguel Marcano <43890220+miguel131299@users.noreply.github.com>
2024-01-17 17:53:02 +0000

    optimzed suboptimal code

--- idp-frontend/src/types.ts
+++
@@ -1,4 +0,0 @@
-export interface Data {
-   id: number
-   name: string
-}
--- idp-frontend/src/utils/apiEndpoints.ts
+++
@@ -1,7 +0,0 @@
-const API_ROOT: string = 'https://ixmk8bqo29.execute-api.eu-west-1.amazonaws.com/dev'
-
-export const ENDPOINTS = {
-   getCountries: `${API_ROOT}/get-countries`,
-   datasetsForACountry: `${API_ROOT}/get-datasets-for-a-country`,
-   triggerRefresh: `${API_ROOT}/trigger-pipeline`
-}
