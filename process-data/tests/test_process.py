270e2380cc186595b6dce048c65dc72d6ac4b433
Lukas Neef <lukas.neef@tum.de>
2024-01-17 18:39:40 +0100

    working tests from top level dir

--- process-data/tests/test_process.py
+++ process-data/tests/test_process.py
@@ -3,25 +3,25 @@ import importlib
 import hxl_parser as parser
 
 def test_fill():
-    test_file = pd.read_excel("tests/test-data/test_fill.xlsx", sheet_name = "Summary Analysis", header=None)
+    test_file = pd.read_excel("process-data/tests/test-data/test_fill.xlsx", sheet_name = "Summary Analysis", header=None)
     processed_data = parser.clean_one_sheet(test_file)
-    verif_data = pd.read_csv("tests/validation-data/test_fill.csv")
+    verif_data = pd.read_csv("process-data/tests/validation-data/test_fill.csv")
     processed_data.reset_index(drop=True, inplace=True)
 
     assert processed_data.compare(verif_data).empty 
 
 def test_edge_hxl_first_row():
-    test_file = pd.read_excel("tests/test-data/test_edge_hxl_first_row.xlsx", sheet_name = "EET Emergency Event Tracking", header=None)
+    test_file = pd.read_excel("process-data/tests/test-data/test_edge_hxl_first_row.xlsx", sheet_name = "EET Emergency Event Tracking", header=None)
     processed_data = parser.clean_one_sheet(test_file)
-    verif_data = pd.read_csv("tests/validation-data/test_edge_hxl_first_row.csv")
+    verif_data = pd.read_csv("process-data/tests/validation-data/test_edge_hxl_first_row.csv")
     processed_data.reset_index(drop=True, inplace=True)
 
     assert processed_data.compare(verif_data).empty
 
 def test_row_at_end():
-    test_file = pd.read_excel("tests/test-data/test_row_at_end.xlsx", sheet_name = "EET Emergency Event Tracking", header=None)
+    test_file = pd.read_excel("process-data/tests/test-data/test_row_at_end.xlsx", sheet_name = "EET Emergency Event Tracking", header=None)
     processed_data = parser.clean_one_sheet(test_file)
-    verif_data = pd.read_csv("tests/validation-data/test_row_at_end.csv")
+    verif_data = pd.read_csv("process-data/tests/validation-data/test_row_at_end.csv")
     processed_data.reset_index(drop=True, inplace=True)
 
     assert processed_data.compare(verif_data).empty
